d3966205c13e5c2e53c416ecc604693bdf6b80ad
Sigit Priadi <sigitpriadi23@gmail.com>
2022-02-25 05:43:01 +0700

    test golang embed file to byte

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -11,4 +13,14 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed image.png
+var image []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("image_new.png", image, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
